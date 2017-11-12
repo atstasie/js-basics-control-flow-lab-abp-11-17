c83c744b8ac6cb28f260266a52ba8c1bf4a34a14
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-12 03:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function scuberGreetingForFeet(ride) {
     return 'This one is on me!';
   } if ((ride > 2000) && (ride < 2500)) {
     return 'I will gladly take your thirty bucks.';
+  } if (ride > 2500) {
+    return 'No can do.'
   }
 }
